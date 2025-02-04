82ec32308df930e1eb243de30c1b39ac96bb025b
Kevin Komro <kkomro1993@gmail.com>
2025-02-04 16:51:38 -0600

    Create operation logic
    Added 3 operation variables
        - operator, num1, num2
    
    Added operate function that calls associated
    operation function based on input (manual for now)

--- calculator.js
+++ calculator.js
@@ -1,11 +1,31 @@
+let firstNumber;
+let secondNumber;
+let operator = "";
+
+
+
+
+
+
+
+
+// Testing ************************
+
 // Add
 console.log('add: ' + add(2,3));
-
 // Subtract
 console.log('subtract: ' + subtract(5, 3));
-
 // Multiply
 console.log("multiply: " + multiply(6, 8));
-
 // Divide
 console.log("divide: " + divide(120, 10));
+console.log("\n");
+
+// Add - using operate
+console.log('operate add: ' + operate('+', 2, 3));
+// Subtract - using operate
+console.log('operate subtract: ' + operate('-', 5, 3));
+// Multiply - using operate
+console.log('operate multiply: ' + operate('*', 6, 8));
+// Divide - using operate
+console.log('operate divide: ' + operate('/', 120, 10));
