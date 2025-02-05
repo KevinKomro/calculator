05a086f824ed6d19d68abe1d2b5bc48670e542eb
Kevin Komro <kkomro1993@gmail.com>
2025-02-05 10:59:52 -0600

    Update display when buttons are clicked
    Added event listeners to each button.
    Digits/operators apply to the result display
    Clear button resets display back to zero.

--- calculator.js
+++ calculator.js
@@ -1,31 +1,39 @@
 let firstNumber;
 let secondNumber;
 let operator = "";
+let result;
 
+let resultElement = document.querySelector(".output-text");
+let buttons = document.querySelectorAll(".inputButton");
+let clearButton = document.querySelector(".clearButton");
 
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    result = resultElement.textContent = event.target.value;
+  });
+});
 
-
-
-
-
+clearButton.addEventListener("click", () => {
+  result = resultElement.textContent = 0;
+});
 
 // Testing ************************
 
-// Add
-console.log('add: ' + add(2,3));
-// Subtract
-console.log('subtract: ' + subtract(5, 3));
-// Multiply
-console.log("multiply: " + multiply(6, 8));
-// Divide
-console.log("divide: " + divide(120, 10));
-console.log("\n");
+// // Add
+// console.log("add: " + add(2, 3));
+// // Subtract
+// console.log("subtract: " + subtract(5, 3));
+// // Multiply
+// console.log("multiply: " + multiply(6, 8));
+// // Divide
+// console.log("divide: " + divide(120, 10));
+// console.log("\n");
 
-// Add - using operate
-console.log('operate add: ' + operate('+', 2, 3));
-// Subtract - using operate
-console.log('operate subtract: ' + operate('-', 5, 3));
-// Multiply - using operate
-console.log('operate multiply: ' + operate('*', 6, 8));
-// Divide - using operate
-console.log('operate divide: ' + operate('/', 120, 10));
+// // Add - using operate
+// console.log("operate add: " + operate("+", 2, 3));
+// // Subtract - using operate
+// console.log("operate subtract: " + operate("-", 5, 3));
+// // Multiply - using operate
+// console.log("operate multiply: " + operate("*", 6, 8));
+// // Divide - using operate
+// console.log("operate divide: " + operate("/", 120, 10));
